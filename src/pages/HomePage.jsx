59481a3cea145e423b806602a0fa23117655d808
jnazario2000 <jnazariorosa2022@my.fit.edu>
2025-09-26 09:53:20 -0400

    changed some formatting stuff

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -26,39 +26,6 @@ function Homepage() {
                     Create Projects
                 </Link>
             </header>
-
-            {/* You can uncomment the below sections if needed later */}
-            {/*
-            <section>
-                <h2>Most Popular</h2>
-                <div className="document-icons">
-                    <div className="doc-icon">📄</div>
-                    <div className="doc-icon">📄</div>
-                    <div className="doc-icon">📄</div>
-                </div>
-            </section>
-
-            <section>
-                <h2>Top Collaborators This Month</h2>
-                <div className="collaborators">
-                    <div className="card">
-                        <h3>Highest Average Code Coverage</h3>
-                        <p>Sample_Collaborator3</p>
-                        <small>Specializes in Web Applications</small>
-                    </div>
-                    <div className="card">
-                        <h3>Most Hours Spent Documenting</h3>
-                        <p>Sample_Collaborator2</p>
-                        <small>Specializes in Python based projects</small>
-                    </div>
-                    <div className="card">
-                        <h3>Highest Rated Collaborator</h3>
-                        <p>Sample_Collaborator3</p>
-                        <small>Specializes in JavaScript based projects</small>
-                    </div>
-                </div>
-            </section>
-            */}
         </div>
     );
 }
